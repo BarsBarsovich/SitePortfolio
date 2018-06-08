7e0128d82d2ce68218f6585af21a5c47a988b6d5
Boris <boris.dryabin@gmail.com>
2018-06-09 01:08:13 +0300

    Сборка спрайтов. Верстка страницы welcome

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,35 @@
 const gulp = require('gulp'),
-      pug = require('gulp-pug'), 
-      postcss = require('gulp-postcss'),
-      sourcemaps = require('gulp-sourcemaps'), 
-      rename = require('gulp-rename'), 
-      del = require('del'), 
-      browserSync = require('browser-sync').create(), 
-      gulpCopy = require('gulp-copy');
+    pug = require('gulp-pug'),
+    postcss = require('gulp-postcss'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rename = require('gulp-rename'),
+    del = require('del'),
+    browserSync = require('browser-sync').create(),
+    gulpCopy = require('gulp-copy');
+
+const $gp = require("gulp-load-plugins")();
 
 const paths = {
-    root :'./dist',
-    templates:{
-        pages : './src/views/pages/*.pug',
-        styles : './src/assets/styles/**/*.scss',
-        dest: './dist'
-    }, 
-    styles:{
+    root: './build',
+    templates: {
+        pages: './src/views/pages/*.pug',
+        styles: './src/assets/styles/**/*.scss',
+        dest: './build'
+    },
+    styles: {
         main: './src/assets/styles/main.scss',
         styles: './src/assets/styles/**/**.scss',
-        dest: './dist/assets/styles'
+        dest: './build/assets/styles'
     },
-    img:{
+    img: {
         main: './src/assets/images/*',
-        dest: './dist/assets/images' 
+        dest: './build/assets/images'
     }
-}      
+}
 
 gulp.task('default', gulp.series(
-    clean, 
-    gulp.parallel(styles,templates, images), 
+    clean,
+    gulp.parallel(styles, templates, images, sprite),
     gulp.parallel(watch, server)
 ));
 
@@ -37,22 +39,22 @@ function watch() {
 }
 
 //compile pug
-function templates(){
+function templates() {
     return gulp.src(paths.templates.pages)
-        .pipe(pug({pretty:true}))
+        .pipe(pug({ pretty: true }))
         .pipe(gulp.dest(paths.templates.dest));
 }
 
 //copy images
-function images(){
+function images() {
     console.log(paths.img.main);
-    return  gulp
-    .src(paths.img.main)
-    .pipe(gulp.dest(paths.img.dest));   
+    return gulp
+        .src(paths.img.main)
+        .pipe(gulp.dest(paths.img.dest));
 }
 
 //compile css
-function styles(){
+function styles() {
     return gulp.src(paths.styles.main)
         .pipe(sourcemaps.init())
         .pipe(postcss(require("./postcss.config")))
@@ -62,7 +64,7 @@ function styles(){
 }
 
 //rmdir
-function clean(){
+function clean() {
     return del(paths.root);
 }
 
@@ -74,7 +76,45 @@ function server() {
     browserSync.watch(paths.root + '/**/*.*', browserSync.reload);
 }
 
+// спрайт иконок
+function sprite() {
+    return gulp.src('./src/assets/images/icons/*.svg')
+        .pipe(
+            $gp.svgmin({
+                js2svg: {
+                    pretty: true
+                }
+            })
+        )
+        .pipe(
+            $gp.cheerio({
+                run($) {
+                    $("[fill], [stroke], [style], [width], [height]")
+                        .removeAttr("fill")
+                        .removeAttr("stroke")
+                        .removeAttr("style")
+                        .removeAttr("width")
+                        .removeAttr("height");
+                },
+                parserOptions: { xmlMode: true }
+            })
+        )
+        .pipe($gp.replace("&gt;", ">"))
+        .pipe(
+            $gp.svgSprite({
+                mode: {
+                    symbol: {
+                        sprite: "../sprite.svg"
+                    }
+                }
+            })
+        )
+        .pipe(gulp.dest('./build/assets/images/icons'));
+}
+
+
 exports.templates = templates;
 exports.styles = styles;
 exports.clean = clean;
 exports.images = images;
+exports.sprite = sprite;
