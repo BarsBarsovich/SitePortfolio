920d83c12d51856a1182d30ed6e78568297bd205
Boris <boris.dryabin@gmail.com>
2018-06-11 03:04:07 +0300

    finished the layout of the skills section

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ const paths = {
         dest: './build/assets/styles'
     },
     img: {
-        main: './src/assets/images/**',
+        main: './src/assets/images/*',
         dest: './build/assets/images'
     }, 
     fonts:{
