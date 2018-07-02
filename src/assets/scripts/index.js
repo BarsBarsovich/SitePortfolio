0305155326de7ef0dc2cf18aa0435794161e621d
BarsBarsovich <boris.dryabin@gmail.com>
2018-07-02 17:07:08 +0300

    form animation

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,13 +1,28 @@
-// import $ from "jquery";
+import $ from "jquery";
 import Vue from "vue";
 import preloader from "./modules/preloader";
 
 new Vue({
   el: "#loginButton",
+  data: {
+    isFront: true
+  },
   beforeMount() {
     preloader();
   },
   methods: {
-    formAnimation: function() {}
+    formAnimation: function() {
+      $(".card__front").toggleClass("card__front--active");
+      $(".card__back").toggleClass("card__back--active");
+      // if (this.isFront) {
+      //   $(".card__front").toggleClass('.card__front--active');
+      //   $(".card__back").show();
+      //   this.isFront = false;
+      // } else {
+      //   $(".card__front").show();
+      //   $(".card__back").hide();
+      //   this.isFront = true;
+      // }
+    }
   }
 });
