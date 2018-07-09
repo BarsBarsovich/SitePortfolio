6058d36298efe87cce8d7d1930cea171c8e7904b
bars <boris.dryabin@gmail.com>
2018-07-09 20:49:27 +0300

    add admin panel. Read\write to blog

--- src/assets/scripts/blog.js
+++ src/assets/scripts/blog.js
@@ -1,11 +1,36 @@
 import "./modules/hamburger";
 import Vue from "vue";
 import preloader from "./modules/preloader";
+import axios from "axios";
 
 new Vue({
   el: "#blog",
+  data: {
+    postList: [],
+    blogPost: {
+      title: "",
+      date: "",
+      content: "",
+      active: ""
+    }
+  },
   beforeMount() {
     preloader();
+    let tempArray = [];
+    axios.get("http://webdev-api.loftschool.com/posts/12").then(response => {
+      for (let item of response.data) {
+        tempArray.push([item.title, item.date, item.content, ""]);
+      }
+      this.postList = tempArray;
+      this.postList[0][4] = 1;
+
+      for (let i = 0; i < this.postList.length; i++) {
+        console.log(this.postList[i]);
+      }
+
+      // this.postList.map(item => console.log(item.title));
+      // console.log(this.postList[0].title);
+    });
   },
   methods: {
     formAnimation: function() {
--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,11 +1,20 @@
 import $ from "jquery";
 import Vue from "vue";
 import preloader from "./modules/preloader";
+import axios from "axios";
 
 new Vue({
   el: "#card",
   data: {
     isFront: true
+  },
+  data() {
+    return {
+      user: {
+        name: "",
+        password: ""
+      }
+    };
   },
   beforeMount() {
     preloader();
@@ -21,6 +30,24 @@ new Vue({
       $(".card__front").toggleClass("card__front--active");
       $(".card__back").toggleClass("card__back--active");
       $("#loginButton").show();
+    },
+    login: function(e) {
+      e.preventDefault();
+      console.log(this.user.name);
+      console.log(this.user.password);
+
+      // e.preventDefault();
+      axios
+        .post("http://webdev-api.loftschool.com/login", this.user)
+        .then(response => {
+          if (response.status === 200) {
+            alert(2);
+            const ttl = Math.floor(Date.now() / 1000 + response.data.ttl);
+            localStorage.setItem("token", response.data.token);
+            localStorage.setItem("ttl", ttl);
+            window.location.href = "./admin";
+          }
+        });
     }
   }
 });
